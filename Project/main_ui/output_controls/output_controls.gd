d2e7f0238188220cbc3b3fe9b3a99e2a099d05c9
Rabbit <rabbit@dfaction.net>
2020-10-25 23:05:49 -0700

    Output view and model now sync with each other.
    No longer have to keep track of every change with the UI or data model backing it. Just call the _sync_with_ui method and everything will sync and update itself. Also added the get_output_path method, which will return the proper path to save the output file, given the path to the input file.

--- Project/main_ui/output_controls/output_controls.gd
+++ Project/main_ui/output_controls/output_controls.gd
@@ -1,15 +1,44 @@
 extends Control
 
 
-export(NodePath) var output_dir_check_btn: NodePath
+export(NodePath) var output_dir_check_box: NodePath
 export(NodePath) var output_dir_line_edit: NodePath
 export(NodePath) var browse_output_dir_btn: NodePath
+export(NodePath) var convert_btn: NodePath
 
-onready var _output_dir_check_btn: CheckButton = get_node(output_dir_check_btn) as CheckButton
+onready var _output_dir_check_box: CheckBox = get_node(output_dir_check_box) as CheckBox
 onready var _output_dir_line_edit: LineEdit = get_node(output_dir_line_edit) as LineEdit
 onready var _browse_output_dir_btn: Button = get_node(browse_output_dir_btn) as Button
+onready var _convert_btn: Button = get_node(convert_btn) as Button
+
+var _use_custom_output_dir: bool
+var _custom_output_dir: String
 
 
-func _on_OutputDirCheckButton_toggled(button_pressed: bool) -> void:
-	_browse_output_dir_btn.disabled = button_pressed
+func _ready() -> void:
+	_sync_with_ui()
+
+
+func get_output_path(for_file: String) -> String:
+	if !_use_custom_output_dir || _custom_output_dir.empty():
+		return "%s.svg" % [for_file.get_basename()]
+
+	return "%s.svg" % [_custom_output_dir.plus_file(for_file.get_file())]
+
+
+func _sync_with_ui() -> void:
+	_use_custom_output_dir = !_output_dir_check_box.pressed
+	_custom_output_dir = _output_dir_line_edit.text
+
+	_browse_output_dir_btn.disabled = !_use_custom_output_dir
+	_convert_btn.disabled = _use_custom_output_dir && _custom_output_dir.empty()
+
+	if _convert_btn.disabled:
+		_convert_btn.hint_tooltip = "Need valid output location to convert files."
+	else:
+		_convert_btn.hint_tooltip = "Convert files into SVG."
+
+
+func _on_OutputDirCheckButton_toggled(_button_pressed: bool) -> void:
+	_sync_with_ui()
 
