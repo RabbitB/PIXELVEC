5fa0d113fcaadedb8d6d4ff6e67767dca38ab2e8
Rabbit <rabbit@dfaction.net>
2020-10-31 11:53:56 -0700

    Wire-up convert files button.

--- Project/main_ui/output_controls/output_controls.gd
+++ Project/main_ui/output_controls/output_controls.gd
@@ -1,20 +1,22 @@
 extends Control
 
 
+signal convert_files()
+
 export(NodePath) var output_dir_dialog: NodePath
 export(NodePath) var output_dir_check_box: NodePath
 export(NodePath) var output_dir_line_edit: NodePath
 export(NodePath) var browse_output_dir_btn: NodePath
 export(NodePath) var convert_btn: NodePath
 
+var _use_custom_output_dir: bool
+var _custom_output_dir: String
+
 onready var _output_dir_dialog: FileDialog = get_node(output_dir_dialog) as FileDialog
 onready var _output_dir_check_box: CheckBox = get_node(output_dir_check_box) as CheckBox
 onready var _output_dir_line_edit: LineEdit = get_node(output_dir_line_edit) as LineEdit
 onready var _browse_output_dir_btn: Button = get_node(browse_output_dir_btn) as Button
 onready var _convert_btn: Button = get_node(convert_btn) as Button
-
-var _use_custom_output_dir: bool
-var _custom_output_dir: String
 
 
 func _ready() -> void:
@@ -55,4 +57,8 @@ func _on_BrowseButton_pressed() -> void:
 func _on_OutputDirDialog_dir_selected(dir: String) -> void:
 	_output_dir_line_edit.text = dir
 	_sync_with_ui()
+
+
+func _on_ConvertButton_pressed() -> void:
+	emit_signal("convert_files")
 
