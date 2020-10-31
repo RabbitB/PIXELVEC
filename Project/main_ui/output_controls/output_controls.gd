4307d79140e6a64d7fef26533c2103a3111b466a
Rabbit <rabbit@dfaction.net>
2020-10-31 11:33:57 -0700

    Reorganize layout of the file output controls

--- Project/main_ui/output_controls/output_controls.gd
+++ Project/main_ui/output_controls/output_controls.gd
@@ -1,11 +1,13 @@
 extends Control
 
 
+export(NodePath) var output_dir_dialog: NodePath
 export(NodePath) var output_dir_check_box: NodePath
 export(NodePath) var output_dir_line_edit: NodePath
 export(NodePath) var browse_output_dir_btn: NodePath
 export(NodePath) var convert_btn: NodePath
 
+onready var _output_dir_dialog: FileDialog = get_node(output_dir_dialog) as FileDialog
 onready var _output_dir_check_box: CheckBox = get_node(output_dir_check_box) as CheckBox
 onready var _output_dir_line_edit: LineEdit = get_node(output_dir_line_edit) as LineEdit
 onready var _browse_output_dir_btn: Button = get_node(browse_output_dir_btn) as Button
@@ -30,6 +32,9 @@ func _sync_with_ui() -> void:
 	_use_custom_output_dir = !_output_dir_check_box.pressed
 	_custom_output_dir = _output_dir_line_edit.text
 
+	_output_dir_line_edit.visible = _use_custom_output_dir
+	_browse_output_dir_btn.visible = _use_custom_output_dir
+
 	_browse_output_dir_btn.disabled = !_use_custom_output_dir
 	_convert_btn.disabled = _use_custom_output_dir && _custom_output_dir.empty()
 
@@ -40,5 +45,14 @@ func _sync_with_ui() -> void:
 
 
 func _on_OutputDirCheckButton_toggled(_button_pressed: bool) -> void:
+	_sync_with_ui()
+
+
+func _on_BrowseButton_pressed() -> void:
+	_output_dir_dialog.popup()
+
+
+func _on_OutputDirDialog_dir_selected(dir: String) -> void:
+	_output_dir_line_edit.text = dir
 	_sync_with_ui()
 
